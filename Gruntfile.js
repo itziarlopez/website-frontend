40c110b58c6a267e5ae83ba89432887675c40870
Barry Steyn <barry.steyn@gmail.com>
2014-12-14 15:43:53 -0800

    Production pipeline ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
     var globalConfig = {
-        output: 'Output'
+        output: 'Output',
+        assets: 'Assets'
     };
 
     grunt.initConfig({
@@ -17,7 +18,8 @@ module.exports = function(grunt) {
         //Deletes folders and files
         clean: {
             all: ['Output', 'Assets/bower_components', 'node_modules'],
-            dev: ['Output']
+            output: ['Output'],
+            temp: ['Assets/temp']
         },
 
         //Copy essential assets needed to render output
@@ -28,44 +30,127 @@ module.exports = function(grunt) {
                 ],
             },
 
-            dev: {
+            assets: {
                 files: [
-                    {expand: true, cwd: 'Assets/bower_components/bootstrap/dist/js/', src: ['bootstrap.js'], dest: '<%= globalConfig.output %>/static/js/', filter: 'isFile'},
-                    {expand: true, cwd: 'Assets/bower_components/jquery/dist/', src: ['jquery.js'], dest: '<%= globalConfig.output %>/static/js/', filter: 'isFile'},
-                    {expand: true, cwd: 'Assets/bower_components/jquery.easing/js/', src: ['jquery.easing.js'], dest: '<%= globalConfig.output %>/static/js/', filter: 'isFile'},
-                    {expand: true, cwd: 'Assets/js/', src: ['navbar.js', 'spamhide.js'], dest: '<%= globalConfig.output %>/static/js/', filter: 'isFile'},
                     {expand: true, cwd: 'Assets', src: ['img/**'], dest: '<%= globalConfig.output %>/static/', filter: 'isFile'},
                 ]
-              },
+            },
+
+            //BootStrap custom - copies customized bootstrap to bootstrap asset folder
+            bootstrap: {
+                files: [
+                    {expand: true, cwd: 'Assets/less', src: ['bootstrap_custom.less'], dest: '<%= globalConfig.assets %>/bower_components/bootstrap/less/', filter: 'isFile'},
+                ]
+            },
+
+            css: {
+                 files: [
+                    {expand: true, cwd: "<%= globalConfig.assets %>/temp", src: ['css/**'], dest: '<%= globalConfig.output %>/static/', filter: 'isFile'},
+                ]               
+            },
+
+            js: {
+                 files: [
+                    {expand: true, cwd: "<%= globalConfig.assets %>/temp", src: ['js/**'], dest: '<%= globalConfig.output %>/static/', filter: 'isFile'},
+                ]               
+            }
         },
 
         //Render less css
         less: {
-          dev: {
-            options: {
-              paths: ["Assets/bower_components/bootstrap"]
+            all: {
+                options: {
+                  paths: [
+                  ]
+                },
+                files: {
+                  "<%= globalConfig.assets %>/temp/css/bootstrap.css": "<%= globalConfig.assets %>/bower_components/bootstrap/less/bootstrap_custom.less",
+                  "<%= globalConfig.assets %>/temp/css/custom.css": "<%= globalConfig.assets %>/less/custom.less"
+                }
             },
-            files: {
-              "<%= globalConfig.output %>/static/css/bootstrap.css": "Assets/bower_components/bootstrap/less/bootstrap.less",
-              "<%= globalConfig.output %>/static/css/custom.css": "Assets/less/custom.less"
-            }
-          },
         },
 
         //Autoprefixer - add browser specific prefixes
         autoprefixer: {
             no_dest: {
-                src: "<%= globalConfig.output %>/static/css/custom.css"
+                src: "<%= globalConfig.assets %>/temp/css/custom.css"
+            }
+        },
+
+        //For Production
+        cssmin: {
+            css: {
+                files: [{
+                    expand: true,
+                    cwd: "<%= globalConfig.assets %>/temp/css/",
+                    src: ["bootstrap.css"],
+                    dest: "<%= globalConfig.output %>/static/css/",
+                },
+                {
+                    expand: true,
+                    cwd: "<%= globalConfig.assets %>/temp/css/",
+                    src: ["custom.css"],
+                    dest: "<%= globalConfig.output %>/static/css/",
+                }]
+            }
+        },
+
+        //Concat - concats files (for production)
+        concat: {
+            js: {
+                src: [  
+                    "<%= globalConfig.assets %>/bower_components/bootstrap/dist/js/bootstrap.js",
+                    "<%= globalConfig.assets %>/bower_components/jquery.easing/js/jquery.easing.js",
+                    "<%= globalConfig.assets %>/js/navbar.js",
+                    "<%= globalConfig.assets %>/js/spamhide.js"
+                ],
+                dest: "<%= globalConfig.assets %>/temp/js/custom.js"
+            }
+        },
+
+        uglify: {
+            js: {
+                src: "<%= globalConfig.assets %>/temp/js/custom.js",
+                dest: "<%= globalConfig.output %>/static/js/custom.js"
             }
         }
 
     });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-auto-install');
+    grunt.loadNpmTasks('grunt-contrib-cssmin'); 
 
-    grunt.registerTask('default', ['auto_install', 'clean:dev', 'copy:theme', 'copy:dev','less:dev','autoprefixer']);
+    grunt.registerTask('default', [
+        'auto_install', 
+        'clean:output',
+        'clean:temp',
+        'copy:theme',
+        'copy:assets',
+        'copy:bootstrap', 
+        'less:all', 
+        'autoprefixer',
+        'copy:css',
+        'concat',
+        'copy:js',
+    ]);
+
+    grunt.registerTask('production', [
+        'auto_install', 
+        'clean:output',
+        'clean:temp',
+        'copy:theme', 
+        'copy:assets',
+        'copy:bootstrap',
+        'less:all',
+        'autoprefixer',
+        'cssmin', 
+        'concat', 
+        'uglify',
+    ]);
 };
